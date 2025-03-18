85f815d15a91aaa55ffbdc8fb2ca156e986fbf50
Aritro Sinha <34037263+sinhaaritro@users.noreply.github.com>
2025-03-18 19:36:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ set -e
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
 # Create the Neovim config directory if it doesn’t exist
-mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/"
+
+# Remove any existing nvim directory or symlink
+rm -rf "$XDG_CONFIG_HOME/nvim"
 
 # Symlink the entire dotfiles repo to ~/.config/nvim
 ln -sf "$PWD" "$XDG_CONFIG_HOME/nvim"
